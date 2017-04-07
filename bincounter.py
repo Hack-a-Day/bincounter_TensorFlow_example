6dbb6ed4170f67de849189bd6a29ccc79c14f6d9
stevendpe <stevend@rimstar.org>
2017-04-07 16:22:45 -0400

    Added copyright notice - have fun with this code!

--- bincounter.py
+++ bincounter.py
@@ -14,7 +14,26 @@
 # after training, see the variables just below these header comments.
 # The same applies if you want to write info for use with TensorBoard.
 #
-# By Steven Dufresne for Hackaday (http://hackaday.com)
+# Copyright (c) 2017 - Steven Dufresne
+#
+# Permission is hereby granted, free of charge, to any person obtaining 
+# a copy of this software and associated documentation files (the 
+# "Software"), to deal in the Software without restriction, including 
+# without limitation the rights to use, copy, modify, merge, publish, 
+# distribute, sublicense, and/or sell copies of the Software, and to 
+# permit persons to whom the Software is furnished to do so, subject 
+# to the following conditions:
+#
+# The above copyright notice and this permission notice shall be 
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
 do_training = 1 # 1 = do the training, 0 = load from file and just run it
